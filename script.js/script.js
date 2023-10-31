6888320cbf6e1ec35da089f540b201001642a8d8
analudms <analuizamelo3m@gmail.com>
2023-10-31 19:07:44 -0300

    creating CREATE - CRUD

--- script.js/script.js
+++ script.js/script.js
@@ -1,14 +1,36 @@
-'use strict'
+"use strict";
 
-const openModal = () => document.getElementById('modal')
-    .classList.add('active')
+const openModal = () =>
+  document.getElementById("modal").classList.add("active");
 
-const closeModal = () => document.getElementById('modal')
-.classList.remove('active')
+const closeModal = () =>
+  document.getElementById("modal").classList.remove("active");
 
+const tempClient = {
+  nome: "Paçoca",
+  email: "pacoca@gmail.com",
+  celular: "31988556699",
+  cidade: "Belo Horizonte",
+};
 
-document.getElementById('registerCustomer')
-        .addEventListener('click', openModal)
+const getLocalStorage = () =>
+    JSON.parse(localStorage.getItem("dbClient")) ?? []; // ?? avalia e retorna o valor da expressão a direita se o valor a esquerda for null ou undefined
 
-document.getElementById('modalClose')
-.addEventListener('click', closeModal)
+const setLocalStorage = (dbClient) =>
+    localStorage.setItem("dbClient", JSON.stringify(dbClient));
+
+//CRUD - create read update delete
+//pegando o que tem no local storage, transformando em json e armazenando na variável cliente / acrescentar no final com push / enviar pro local storage com dados e valores
+// -------- CREATE --------
+const createCliente = (client) => {
+    const dbClient = getLocalStorage()
+    dbClient.push(client);
+    setLocalStorage(dbClient)
+};
+
+//Eventos
+document
+  .getElementById("registerCustomer")
+  .addEventListener("click", openModal);
+
+document.getElementById("modalClose").addEventListener("click", closeModal);
